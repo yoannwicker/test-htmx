4a327b2bbc6d211461e23e6b3e74f82628e69dba
ywicker <yoann.wicker@vidal.fr>
2024-05-29 13:31:19 +0200

    clear unused setter

--- src/main/java/com/model/Task.java
+++ src/main/java/com/model/Task.java
@@ -13,13 +13,8 @@ public class Task {
     private Long id;
     private String content;
 
-    // Getters and Setters
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getContent() {
